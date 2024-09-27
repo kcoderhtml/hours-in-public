7c618e9348ca1a2c1aa7147ce280301ba731eec4
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-09-27 08:31:00 -0400

    feat: add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,16 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [
+    tailwind({ applyBaseStyles: false }),
+    icon({
+      include: {
+        mdi: ["heart"],
+      },
+    }),
+  ],
 });
